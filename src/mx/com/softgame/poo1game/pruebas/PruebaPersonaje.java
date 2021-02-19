f45da2225c1d359def5b65fbf81bf6f9bb144366
Leonardo373 <garcianievesleonardo@gmail.com>
2021-02-19 09:26:25 -0600

    Initial commit

--- src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -2,16 +2,20 @@ package mx.com.softgame.poo1game.pruebas;
 import mx.com.softgame.poo1game.personajes.*;
 public class PruebaPersonaje{
 	public static void main(String[] args){
-		Personaje alumno = new Personaje();
 		Personaje per01 = new Personaje("Aline", 200);
-		per01.getDetalle();
 		Personaje per02 = new Personaje("Erick", 10);
-		per02.getDetalle();
 		Personaje per03 = new Personaje("Alan", 1);
-		per03.getDetalle();
 		Personaje per04 = new Personaje("Lesly", -10);
-		per04.getDetalle();
 		Personaje per05 = new Personaje("Brian", 500);
-		per05.getDetalle();
+		System.out.println(per01.getDetalle());
+		System.out.println(per02.getDetalle());
+		System.out.println(per03.getDetalle());
+		System.out.println(per04.getDetalle());
+		System.out.println(per05.getDetalle());
+		Personaje per06 = per05;
+		Personaje per07 = per04;
+		Personaje per08 = per03;
+		Personaje per09 = per02;
+		Personaje per10 = per01;
 	}
 }

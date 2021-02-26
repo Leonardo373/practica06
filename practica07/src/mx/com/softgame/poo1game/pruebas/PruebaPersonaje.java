0c924382871b58f2675c9916cf910e3147e60f77
Leonardo373 <garcianievesleonardo@gmail.com>
2021-02-26 08:55:09 -0600

    Ciclo do

--- practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -6,8 +6,9 @@ public class PruebaPersonaje{
 		Personaje per02 = new Personaje("Erick");
 		Personaje per03 = new Personaje("Wendy");
 		Personaje per04 = new Personaje("Lesly");
+		outfor:
 		for (int i = 0;i < 3 ;i++) {
-			int ed = (int)(Math.random()*250);
+			ed = (int)(Math.random()*250);
 			if (per01.setEdad(ed)) {
 				System.out.println(per01.getDetalle()"se modificó la edad");
 			}else{
@@ -24,24 +25,14 @@ public class PruebaPersonaje{
 				System.out.println(per03.getDetalle()"sin modificar en edad a"+ed);
 			}
 		}
-		System.out.println(per01.getDetalle());
-		System.out.println(per02.getDetalle());
-		System.out.println(per03.getDetalle());
-		System.out.println(per04.getDetalle());
-		Personaje per07 = per04;
-		Personaje per08 = per03;
-		Personaje per09 = per02;
-		Personaje per10 = per01;
-		per08.setNombre("Yosselin");
-		per10.setNombre("Ana");
-		int rand1 = (int)(Math.random()*1000);
-		int rand2 = (int)(Math.random()*1000);
-		per07.setEdad(rand1);
-		per09.setEdad(rand2);
-		System.out.println("================");
-		System.out.println(per01.getDetalle());
-		System.out.println(per02.getDetalle());
-		System.out.println(per03.getDetalle());
-		System.out.println(per04.getDetalle());
+		do{
+			ed = (int)(Math.random()*250);
+			System.out.println(ed);
+			if (ed == 150) {
+				System.out.println("***Bingo***");
+				continue outfor;
+			}
+		}while(!per04.setEdad(ed));
+		System.out.println(per04.getDetalle()+"\n");
 	}
 }

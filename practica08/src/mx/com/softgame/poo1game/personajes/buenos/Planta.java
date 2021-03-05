1c282e079bab6835bafc30dcfbb0beb4aaa12c9a
Leonardo373 <garcianievesleonardo@gmail.com>
2021-03-05 09:29:25 -0600

    Constructor Planta1

--- practica08/src/mx/com/softgame/poo1game/personajes/buenos/Planta.java
+++ practica08/src/mx/com/softgame/poo1game/personajes/buenos/Planta.java
@@ -2,4 +2,14 @@ package mx.com.softgame.poo1game.personajes.buenos;
 import mx.com.softgame.poo1game.personajes.*;
 public class Planta extends Personaje{
 	char escudo;
+
+	public char getEscudo(){
+		return escudo;
+	}
+
+	public Planta(String nombre, int vida, char escudo){
+		this.nombre = nombre;
+		this.vida = vida;
+		this.escudo = escudo;
+	}
 }

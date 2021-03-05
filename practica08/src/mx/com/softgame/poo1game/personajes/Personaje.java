c70f7b2d9bb28e5c64d36d2883b936eba33d976f
Leonardo373 <garcianievesleonardo@gmail.com>
2021-03-05 07:40:56 -0600

    Inicio

---
+++ practica08/src/mx/com/softgame/poo1game/personajes/Personaje.java
@@ -0,0 +1,36 @@
+/*
+Nombre: Leonardo García Nieves
+Hora de inicio: 7:40 a.m.
+Hora de finalización:  a.m.
+*/
+package mx.com.softgame.poo1game.personajes;
+public class Personaje{
+	private String nombre;
+	private int vida;
+	public Personaje (String nombre){
+		this.nombre = nombre;
+		this vida = 0;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public int getVida(){
+		return vida;
+	}
+	public boolean setVida(int vida){
+		if  (vida >= 0 && vida <= 100){
+			this vida = vida;
+			return true;
+		} else{
+			return false;
+		}
+	}
+	public void setNombre(String nombre){
+		if (nombre.length() > 3 && nombre.length() < 10){
+			this.nombre = nombre;
+		}
+	}
+	public String getDetalle(){
+		return nombre + "\t" + vida;
+	}
+}
---
+++ practica08/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -0,0 +1,45 @@
+/*
+Nombre: Leonardo García Nieves
+Hora de inicio: 7:40 a.m.
+Hora de finalización: 9:32 a.m.
+*/
+package mx.com.softgame.poo1game.pruebas;
+import mx.com.softgame.poo1game.personajes.*;
+public class PruebaPersonaje{
+	public static void main(String[] args){
+		Personaje per01 = new Personaje("Omar");
+		Personaje per02 = new Personaje("Erick");
+		Personaje per03 = new Personaje("Wendy");
+		Personaje per04 = new Personaje("Lesly");
+		outfor:
+		for (int i = 0;i < 3 ;i++) {
+			int ed = (int)(Math.random()*250);
+			if (per01.setVida(ed)) {
+				System.out.println(per01.getDetalle()+" se modifico la vida");
+			}else{
+				System.out.println(per01.getDetalle()+" sin modificar en vida a "+ed);
+			}
+			ed = (int)(Math.random()*250);
+			if (per02.setVida(ed)) {
+				System.out.println(per02.getDetalle()+" se modifico la vida");
+			}else{
+				System.out.println(per02.getDetalle()+" sin modificar en vida a "+ed);
+			}
+			ed = (int)(Math.random()*250);
+			if (per03.setVida(ed)) {
+				System.out.println(per03.getDetalle()+" se modifico la vida");
+			}else{
+				System.out.println(per03.getDetalle()+" sin modificar en vida a "+ed);
+			}
+			do{
+				ed = (int)(Math.random()*250);
+				System.out.println(ed);
+				if (ed == 150) {
+					System.out.println("***Bingo***");
+					continue outfor;
+				}
+			}while(!per04.setVida(ed));
+			System.out.println(per04.getDetalle());
+		}
+	}
+}

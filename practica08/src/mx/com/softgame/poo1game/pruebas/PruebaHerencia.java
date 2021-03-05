54efeb0ece5e07c14b4f119e4336562de871f27f
Leonardo373 <garcianievesleonardo@gmail.com>
2021-03-05 10:10:30 -0600

    PruebaHerencia

---
+++ practica08/src/mx/com/softgame/poo1game/pruebas/PruebaHerencia.java
@@ -0,0 +1,4 @@
+package mx.com.softgame.poo1game.prueba;
+import mx.com.softgame.poo1game.personajes.buenos.*;
+import mx.com.softgame.poo1game.personajes.malos.*;
+import mx.com.softgame.poo1game.personajes.*;

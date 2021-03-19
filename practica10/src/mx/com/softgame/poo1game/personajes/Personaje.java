9e7c73f77bd70ba4eab9a808802ed7c93c4267f1
Leonardo373 <garcianievesleonardo@gmail.com>
2021-03-19 07:45:00 -0600

    Copia Practica9 7:44
    Se realizó la copia de la practica anterior 7:44

---
+++ practica10/src/mx/com/softgame/poo1game/personajes/Personaje.java
@@ -0,0 +1,55 @@
+/*
+Nombre: Leonardo García Nieves
+Hora de inicio: 7:35 a.m.
+Hora de finalización: 00:00 a.m.
+*/
+package mx.com.softgame.poo1game.personajes;
+public class Personaje{
+	protected String nombre;
+	protected int vida;
+	public Personaje (String nombre, int vida){
+		this.nombre = nombre;
+		this.vida = vida;
+	}
+	public Personaje (String nombre){
+		this(nombre, 3);
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public int getVida(){
+		return vida;
+	}
+	public boolean setVida(int vida){
+		if  (vida >= 0 && vida <= 100){
+			this.vida = vida;
+			return true;
+		} else{
+			return false;
+		}
+	}
+	public void setNombre(String nombre){
+		if (nombre.length() > 3 && nombre.length() < 10){
+			this.nombre = nombre;
+		}
+	}
+	public String toString(){
+		return nombre + "\t" + vida;
+	}
+	public void decVida(){
+		decVida(1);
+	}
+	public void decVida(int resta){
+		if ((vida - resta)>0) {
+			vida = vida - resta;
+		}
+	}
+	public boolean equals(Object o){
+		boolean result = false;
+		Personaje x = (Personaje) o;
+		if ((nombre == x.nombre) && (vida == x.vida)){
+			result = true;
+		}
+		return result;
+	}
+}
---
+++ practica10/src/mx/com/softgame/poo1game/personajes/malos/Zombie.java
@@ -0,0 +1,44 @@
+package mx.com.softgame.poo1game.personajes.malos;
+import mx.com.softgame.poo1game.personajes.*;
+public class Zombie extends Personaje{
+	private boolean ataque;
+	public boolean getAtaque(){
+		return ataque;
+	}
+	public Zombie(String nombre, int vida, boolean ataque){
+		super(nombre, vida);
+		this.ataque = ataque;
+	}
+	public Zombie(String nombre, boolean ataque){
+		this(nombre, 3, ataque);
+	}
+	public Zombie(String nombre){
+		this(nombre, 3, false);
+	}
+	public String toString(){
+		return super.toString()+" "+ataque;
+	}
+	public void decVida(){
+		if (ataque == false) {
+			super.decVida(3);
+		}else{
+			super.decVida(2);
+		}
+	}
+	public void decVida(int resta){
+		if (ataque == false) {
+			super.decVida(resta*3);
+		}else{
+			super.decVida(resta*2);
+		}
+	}
+	public boolean equals(Object m){
+		boolean result = false;
+		super.equals(m);
+		Zombie z = (Zombie) m;
+		if (ataque == z.ataque){
+			result = true;
+		}
+		return result;
+	}
+}

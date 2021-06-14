e6f3617ce41ddef4ca1c0f4244e646a43044c417
Leonardo373 <garcianievesleonardo@gmail.com>
2021-06-14 09:50:02 -0500

    P3. Clases de Ordenamiento
    Se crearon las clases de ordenamiento

--- extraGarcia/src/com/micel/mensajeria/utileria/CmpName.java
+++ extraGarcia/src/com/micel/mensajeria/utileria/CmpName.java
@@ -1,5 +1,20 @@
 package com.micel.mensajeria.utileria;
+import com.micel.mensajeria.mensajes.*;
+import java.util.*;
 
-public class CmpName{
-	
+public class CmpName <Mensaje> implements Mensaje{
+
+	public int compareTo (Object o){
+        double f = super.getName() - ((Mensaje)o).super.getName();
+        if (f == 0.0){
+            return 0;
+        }
+        else if (f<0.0){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    }
+
 }
--- extraGarcia/src/com/micel/mensajeria/utileria/CmpNumber.java
+++ extraGarcia/src/com/micel/mensajeria/utileria/CmpNumber.java
@@ -1,5 +1,20 @@
 package com.micel.mensajeria.utileria;
+import com.micel.mensajeria.mensajes.*;
+import java.util.*;
 
-public class CmpNumber{
+public class CmpNumber <Mensaje> implements Mensaje{
+
+	public int compareTo (Object o){
+        double f = super.getNumber() - ((Mensaje)o).super.getNumber();
+        if (f == 0.0){
+            return 0;
+        }
+        else if (f<0.0){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    }
 
 }
